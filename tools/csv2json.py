d3431eed132371ebba0b2556d7558d1872514f77
Henry Todd <henry@hjst.org>
2020-07-26 16:09:16 +0100

    Add initial version of csv2json.py

---
+++ tools/csv2json.py
@@ -0,0 +1,49 @@
+import csv
+import json
+import sys
+import argparse
+import datetime
+
+parser = argparse.ArgumentParser(description='Convert flood archive CSV data to JSON "dayfile" format.',
+                                 epilog='Reads CSV input on stdin and writes JSON to stdout.')
+parser.add_argument('measure', metavar='MEASURE', type=str, nargs=1,
+                    help='the measure id whose readings you want to include in the conversion')
+args = parser.parse_args()
+measure_id = args.measure[0]
+
+hits = list()
+headers = list()
+line_counter = 0
+reader = csv.reader(sys.stdin)
+
+for row in reader:
+    if line_counter == 0:
+        headers = row
+    line_counter += 1
+    if any(measure_id in field for field in row):
+        hits.append(row)
+
+output_obj = {
+    'meta': {
+        'hfdflood': {
+            'generator': 'csv2json.py',
+            'created_at': datetime.datetime.now().isoformat(),
+            'version': 1.0,
+            'measure_id': measure_id,
+            'csv_source': 'https://environment.data.gov.uk/flood-monitoring/archive'
+        }
+    },
+    'items': []
+}
+
+for reading in hits:
+    output_obj['items'].append({
+        'dateTime': reading[headers.index('dateTime')],
+        'value': reading[headers.index('value')]
+    })
+
+print(json.dumps(output_obj), file=sys.stdout)
+
+print(f"Headers: {headers}", file=sys.stderr)
+print(f"Number of hits: {len(hits)}", file=sys.stderr)
+print(f"Number of CSV input lines: {line_counter}", file=sys.stderr)
